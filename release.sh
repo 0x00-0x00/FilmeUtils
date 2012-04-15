d184288a3b9b1c6f20e1a3eedbcc8a93f108102b
Lucas <lucass@gmail.com>
2012-04-15 17:16:53 -0300

    release script

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+rm release.properties
 VERSION=$(cat pom.xml | grep "<version>.*-SNAPSHOT" | grep -o "[0-9.]*")
+VERSION_FILE=src/main/java/filmeUtils/MainCLI.java
 echo $VERSION
-sed -i "s/VERSION = \"[0-9\.]*\";/VERSION = \"$VERSION\";/g" src/main/java/filmeUtils/MainCLI.java
+sed -i "s/VERSION = \"[0-9\.]*\";/VERSION = \"$VERSION\";/g" $VERSION_FILE
+git add $VERSION_FILE
+git commit -m"New version"
 mvn3 clean package release:prepare
 mvn3 release:perform -Dgoals=deploy

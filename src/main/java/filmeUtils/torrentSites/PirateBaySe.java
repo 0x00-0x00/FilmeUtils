9806f46d54c20ca39dcc56f8c731a13945d0b962
Lucas <lucass@gmail.com>
2012-04-18 00:11:26 -0300

    Nome de legenda bizarro estava quebrando pirate bay search

--- src/main/java/filmeUtils/torrentSites/PirateBaySe.java
+++ src/main/java/filmeUtils/torrentSites/PirateBaySe.java
@@ -1,5 +1,8 @@
 package filmeUtils.torrentSites;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.apache.http.client.methods.HttpGet;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,15 +22,20 @@ public class PirateBaySe implements TorrentSite {
 		this.httpclient = httpclient;
 	}
 
-	/* (non-Javadoc)
-	 * @see filmeUtils.torrentSites.TorrentSite#getMagnetLinkFirstResultOrNull(java.lang.String)
-	 */
 	public String getMagnetLinkFirstResultOrNull(final String exactFileName){
-		final HttpGet httpGet = new HttpGet(THEPIRATEBAY_SE_SEARCH_URL+exactFileName+MORESEEDS_SEARCH_URL);
+		final String encoded;
+		try {
+			encoded = URLEncoder.encode(exactFileName,"UTF-8");
+		} catch (final UnsupportedEncodingException e1) {
+			throw new RuntimeException("Never gonna happen",e1);
+		}
+		final String uri = THEPIRATEBAY_SE_SEARCH_URL+encoded+MORESEEDS_SEARCH_URL;
+		System.out.println(uri);
+		final HttpGet httpGet = new HttpGet(uri);
 		String searchResult;
 		try {
 			searchResult = httpclient.executeAndGetResponseContents(httpGet);
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			e.printStackTrace();
 			return null;
 		}

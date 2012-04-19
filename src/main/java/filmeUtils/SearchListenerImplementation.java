84032df4623348b6d2eba5c41c43aadbab7e5f09
Lucas <lucass@gmail.com>
2012-04-19 00:53:35 -0300

    refatorando sem testes tsc tsc tsc

--- src/main/java/filmeUtils/SearchListenerImplementation.java
+++ src/main/java/filmeUtils/SearchListenerImplementation.java
@@ -2,20 +2,15 @@ package filmeUtils;
 
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.ZipException;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.conn.ConnectionPoolTimeoutException;
 
-import com.github.junrar.testutil.ExtractArchive;
-
+import filmeUtils.extraction.ExtractorImpl;
 import filmeUtils.http.SimpleHttpClientImpl;
 import filmeUtils.torrentSites.TorrentSearcher;
 
@@ -49,59 +44,62 @@ final class SearchListenerImplementation implements SearchListener {
 	}
 
 	private void unzipAndPrint(final String name,final String link){
-		String contentType = "YOU SHOULD NEVER SEE THIS, I'm so embarassed...";
 		try {
 			final String validNameForFile = name.replaceAll("[/ \\\\?]", "_");
-			final File currentSubtitleCollection = new File(subtitleDestination, validNameForFile);
-			currentSubtitleCollection.mkdir();
-			System.out.println("Extraindo legendas para "+currentSubtitleCollection.getAbsolutePath());
-			final File destFile = new File(currentSubtitleCollection,"compressedSubs");
-			destFile.createNewFile();
+			final File currentSubtitleFolder = new File(subtitleDestination, validNameForFile);
+			currentSubtitleFolder.mkdir();
+			System.out.println("Extraindo legendas para "+currentSubtitleFolder.getAbsolutePath());
 			
-	    	contentType = httpclient.getToFile(link, destFile);
-			
-			if(contentType.contains("rar")){
-				ExtractArchive.extractArchive(destFile, currentSubtitleCollection);
-			}
-			if(contentType.contains("zip")){
-				final ZipFile zipFile = new ZipFile(destFile);
-				
-				final Enumeration<? extends ZipEntry> entries = zipFile.entries();
-				while(entries.hasMoreElements()) {
-					final ZipEntry entry = entries.nextElement();
-					final File unzippingFile = new File(currentSubtitleCollection,entry.getName());
-					if(entry.isDirectory()) {
-						unzippingFile.mkdir();
-					}else{
-						unzippingFile.createNewFile();
-						final FileOutputStream fileOutputStream = new FileOutputStream(unzippingFile);
-						final InputStream inputStream = zipFile.getInputStream(entry);
-						IOUtils.copy(inputStream, fileOutputStream);
-						inputStream.close();
-						fileOutputStream.close();
-					}
-				}
-				zipFile.close();
-				destFile.delete();
-			}
-			
-			if(contentType.contains("text/html")){
-				final String fileText = FileUtils.readFileToString(destFile);
-				System.out.println(fileText);
-			}
-			
-			@SuppressWarnings("unchecked")
-			final Iterator<File> iterateFiles = FileUtils.iterateFiles(currentSubtitleCollection, new String[]{"srt"}, true);
-			while(iterateFiles.hasNext()){
-				final File next = iterateFiles.next();
-				showFileName(next);
-			}
+			downloadLinkToFolder(link, currentSubtitleFolder);
+	    	
+			outputSubtitleFiles(currentSubtitleFolder);
 			
 		} catch(final ConnectionPoolTimeoutException e){
 			System.out.println("Tempo máximo de requisição atingido ("+SimpleHttpClientImpl.TIMEOUT+" segundos)");
 		}catch (final IOException e) {
-			System.out.println(contentType);
 			e.printStackTrace();//not the end of the world as we know it, and I fell fine
+		}
+	}
+
+	private void downloadLinkToFolder(final String link, final File folder) throws IOException, ClientProtocolException, ZipException {
+		String contentType;
+		final File destFile = createFileToBeWritten(folder);
+		
+		contentType = httpclient.getToFile(link, destFile);
+		extract(destFile, folder, contentType);
+		
+		destFile.delete();
+	}
+
+	private File createFileToBeWritten(final File currentSubtitleCollection)
+			throws IOException {
+		final File destFile = new File(currentSubtitleCollection,"compressedSubs");
+		destFile.createNewFile();
+		return destFile;
+	}
+
+	private void outputSubtitleFiles(final File currentSubtitleFolder) {
+		@SuppressWarnings("unchecked")
+		final Iterator<File> iterateFiles = FileUtils.iterateFiles(currentSubtitleFolder, new String[]{"srt"}, true);
+		while(iterateFiles.hasNext()){
+			final File next = iterateFiles.next();
+			showFileName(next);
+		}
+	}
+
+	private void extract(final File compressedFile,
+			final File destinationFolder, final String contentType)
+			throws ZipException, IOException {
+		final ExtractorImpl extract = new ExtractorImpl();
+		if(contentType.contains("rar")){
+			extract.unrar(compressedFile, destinationFolder);
+		}
+		if(contentType.contains("zip")){
+			extract.unzip(compressedFile, destinationFolder);
+		}
+		if(contentType.contains("text/html")){
+			final String fileText = FileUtils.readFileToString(compressedFile);
+			System.out.println(fileText);
 		}
 	}
 

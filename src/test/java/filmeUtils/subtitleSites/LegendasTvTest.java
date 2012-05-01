56e25f8ccd0ede0208defec9eb5282aee1934319
Lucas <lucass@gmail.com>
2012-05-01 20:51:24 -0300

    simpleSearchWithMoreThanOneResultOnePageLazyTest simpleSearchWithMoreThanOneResultOnePageLazyIgnoreFirstTest simpleSearchWithMoreThanOneResultTwoPagesGreedyTest

--- src/test/java/filmeUtils/subtitleSites/LegendasTvTest.java
+++ src/test/java/filmeUtils/subtitleSites/LegendasTvTest.java
@@ -60,4 +60,68 @@ public class LegendasTvTest {
 		});
 		Assert.assertEquals(0, expectedResults.size());
 	}
+	
+	@Test
+	public void simpleSearchWithMoreThanOneResultOnePageLazyTest(){
+		final String response = "LegendasTvLotsOfResultsOnePage.html";
+		mock.setResponseForUrl("http://legendas.tv/index.php?opcao=buscarlegenda&pagina=1", response);
+		final ArgumentsParserMock cli = new ArgumentsParserMock();
+		cli.isLazy = true;
+		final LegendasTv subject = new LegendasTv(cli,mock, dummyOutputListener);
+		final AtomicBoolean wasCalled = new AtomicBoolean(false);
+		subject.search("foo", new SearchListener() {
+			public boolean foundReturnSuccess(final String name, final String link) {
+				Assert.assertEquals("Community.S01.Complete.HDTV (PACK DE LEGENDAS)", name);
+				Assert.assertEquals("http://legendas.tv/info.php?c=1&d=ae752094b5a1fc977e933fb619527d1a", link);
+				wasCalled.set(true);
+				return true;
+			}
+		});
+		Assert.assertTrue(wasCalled.get());
+	}
+	
+	@Test
+	public void simpleSearchWithMoreThanOneResultOnePageLazyIgnoreFirstTest(){
+		final String response = "LegendasTvLotsOfResultsOnePage.html";
+		mock.setResponseForUrl("http://legendas.tv/index.php?opcao=buscarlegenda&pagina=1", response);
+		final ArgumentsParserMock cli = new ArgumentsParserMock();
+		cli.isLazy = true;
+		final LegendasTv subject = new LegendasTv(cli,mock, dummyOutputListener);
+		final AtomicBoolean wasCalled = new AtomicBoolean(false);
+		subject.search("foo", new SearchListener() {
+			public boolean foundReturnSuccess(final String name, final String link) {
+				if(name.equals("Community.S01.Complete.HDTV (PACK DE LEGENDAS)")){
+					return false;
+				}
+				Assert.assertEquals("Community.S01.720p.WEB-DL.DD5.1.H.264-myTV/HoodBag (PACK DE LEGENDAS)", name);
+				Assert.assertEquals("http://legendas.tv/info.php?c=1&d=a7faf31ace51a2110b69b822ff84b434", link);
+				wasCalled.set(true);
+				return true;
+			}
+		});
+		Assert.assertTrue(wasCalled.get());
+	}
+	
+	@Test
+	public void simpleSearchWithMoreThanOneResultTwoPagesGreedyTest(){
+		mock.setResponseForUrl("http://legendas.tv/index.php?opcao=buscarlegenda&pagina=1", "LegendasTvLotsOfResultsTwoPages.html");
+		mock.setResponseForUrl("http://legendas.tv/index.php?opcao=buscarlegenda&pagina=2", "LegendasTvOneResult.html");
+		final ArgumentsParserMock cli = new ArgumentsParserMock();
+		cli.isLazy = false;
+		final LegendasTv subject = new LegendasTv(cli,mock, dummyOutputListener);
+		final Map<String, String> expectedResults = new LinkedHashMap<String, String>();
+		expectedResults.put("Community.S01.Complete.HDTV (PACK DE LEGENDAS)", "http://legendas.tv/info.php?c=1&d=ae752094b5a1fc977e933fb619527d1a");
+		expectedResults.put("Community.S01.720p.WEB-DL.DD5.1.H.264-myTV/HoodBag (PACK DE LEGENDAS)","http://legendas.tv/info.php?c=1&d=a7faf31ace51a2110b69b822ff84b434");
+		expectedResults.put("Community.S02.720p.WEB-DL.DD5.1.H.264-HoodBag (PACK DE LEGENDAS)","http://legendas.tv/info.php?c=1&d=fe19c9afc9dbf6a53a74d0782da8861a");
+		expectedResults.put("Castle.S04E21.720p.WEB-DL.DD5.1.H.264-NFHD","http://legendas.tv/info.php?c=1&d=e613c192c4279ff32db5f3ad0640e8d0");
+		subject.search("foo", new SearchListener() {
+			public boolean foundReturnSuccess(final String name, final String link) {
+				final String linkExpected = expectedResults.get(name);
+				Assert.assertEquals("For "+name,linkExpected, link);
+				expectedResults.remove(name);
+				return true;
+			}
+		});
+		Assert.assertEquals(0, expectedResults.size());
+	}
 }

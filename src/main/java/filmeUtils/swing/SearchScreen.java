91ee935dc1c9448d7e79c827e66ee34fbb08e145
Lucas Santos <lcsantos@objective.com.br>
2012-05-14 14:11:05 -0300

    organize imports

--- src/main/java/filmeUtils/swing/SearchScreen.java
+++ src/main/java/filmeUtils/swing/SearchScreen.java
@@ -10,7 +10,6 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;

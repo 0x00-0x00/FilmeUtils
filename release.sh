1fe10fb3b88df9e1472c110a6a1429b652ac6c2d
Lucas <lucass@gmail.com>
2013-04-26 13:01:26 -0300

    Release escreve versão no readme

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 rm release.properties
+VERSION=$(cat pom.xml | grep "<version>.*-SNAPSHOT" | grep -o "[0-9.]*")
+VERSION_FILE=README.md
+echo $VERSION
+sed -i "s/v[0-9]\.[0-9]/v$VERSION/g" $VERSION_FILE
+git add $VERSION_FILE
+git commit -m"New version on readme"
 mvn clean package release:prepare
 mvn release:perform 
-#cp target/filmeUtils.jar filmeUtils-$VERSION.jar
+rm release.properties

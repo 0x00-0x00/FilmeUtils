1051f4ab7fdc05f6b8e719bbd3069309af7d52e9
Lucas <lucass@gmail.com>
2013-08-03 16:10:19 -0300

    Arrumando release script, versao vem do manifest agora

--- release.sh
+++ release.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 rm release.properties
-VERSION=$(cat pom.xml | grep "<version>.*-SNAPSHOT" | grep -o "[0-9.]*")
-VERSION_FILE=README.md
-echo $VERSION
-sed -i "s/v[0-9]\.[0-9]/v$VERSION/g" $VERSION_FILE
-git add $VERSION_FILE
-git commit -m"New version on readme"
 mvn clean package release:prepare
 mvn release:perform 
 rm release.properties

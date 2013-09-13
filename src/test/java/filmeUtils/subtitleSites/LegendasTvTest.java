5c1bd024951f628660bed9d4d121d5d4d41df8f1
Lucas <lucass@gmail.com>
2013-09-12 23:53:31 -0300

    Ops, testes quebrando

--- src/test/java/filmeUtils/subtitleSites/LegendasTvTest.java
+++ src/test/java/filmeUtils/subtitleSites/LegendasTvTest.java
@@ -27,10 +27,10 @@ public class LegendasTvTest {
 		mock.setResponseForUrl("http://legendas.tv/util/carrega_legendas_busca/termo:house%20of%20the%20dead/page:2", "LegendasTvOneResult.html");
 		final LegendasTv subject = new LegendasTv(mock, dummyOutputListener);
 		final Map<String, String> expectedResults = new LinkedHashMap<String, String>();
-		expectedResults.put("House [2008]DVDRip[Xvid AC3[5.1]-RoCK&BlueLadyRG", "http://legendas.tv/pages/downloadarquivo/c214ba8d3a08aa5ed2e6d0c228cb7c08");
-		expectedResults.put("House.(1986).DVDRip.XviD-DitriS", "http://legendas.tv/pages/downloadarquivo/2a385576ec54161d101d59aeae496bbf");
+		expectedResults.put("House [2008]DVDRip[Xvid AC3[5 1]-RoCK&BlueLadyRG", "http://legendas.tv/pages/downloadarquivo/c214ba8d3a08aa5ed2e6d0c228cb7c08");
+		expectedResults.put("House (1986) DVDRip XviD-DitriS", "http://legendas.tv/pages/downloadarquivo/2a385576ec54161d101d59aeae496bbf");
 		expectedResults.put("House (1986) Xvid [ENG] Dvdrip", "http://legendas.tv/pages/downloadarquivo/b9348478cdbca402907acfb6ab509874");
-		expectedResults.put("Breaking.Bad.S05E13.HDTV.x264-EVOLVE-AFG-mSD-ChameE-IMMERSE-BS", "http://legendas.tv/pages/downloadarquivo/137620a9b17d9065bbcad03a9e3feaf7");
+		expectedResults.put("Breaking Bad S05E13 HDTV x264-EVOLVE-AFG-mSD-ChameE-IMMERSE-BS", "http://legendas.tv/pages/downloadarquivo/137620a9b17d9065bbcad03a9e3feaf7");
 		subject.search("house of the dead", new SubtitleLinkSearchCallback(){@Override public void process(final SubtitlePackageAndLink subAndLink) {
 			final String name = subAndLink.name;
 			final String link = subAndLink.link;
@@ -47,10 +47,10 @@ public class LegendasTvTest {
 		mock.setResponseForUrl("http://legendas.tv/util/carrega_destaques/series/page:2", "LegendasTvNewSeriesSecondPage.html");
 		final LegendasTv subject = new LegendasTv(mock, dummyOutputListener);
 		final Map<String, String> expectedResults = new LinkedHashMap<String, String>();
-		expectedResults.put("Sons.of.Anarchy.S06E01.PROPER.HDTV.x264.ASAP.REPACK.AFG.REPACK.FUM.REPACK.EVOLVE", "http://legendas.tv/pages/downloadarquivo/d5179f666b2d04c77b0c4de496050860");
-		expectedResults.put("Suits.S03E09.HDTV.x264.EVOLVE.AFG.EVOLVE.mSD.HWD", "http://legendas.tv/pages/downloadarquivo/7b7a85a10c14185754a0c599a6e8163b");
-		expectedResults.put("Paranormal.Witness.S03E11.HDTV.x264.INNOCENCE.AFG.INNOCENCE", "http://legendas.tv/pages/downloadarquivo/1d22e8cb551426fde59db248607b64fd");
-		expectedResults.put("Hell.on.Wheels.S03E06.HDTV.x264.EVOLVE.AFG.EVOLVE", "http://legendas.tv/pages/downloadarquivo/3be6ce89d4aa35652875358cf441badc");
+		expectedResults.put("Sons of Anarchy S06E01 PROPER HDTV x264 ASAP REPACK AFG REPACK FUM REPACK EVOLVE", "http://legendas.tv/pages/downloadarquivo/d5179f666b2d04c77b0c4de496050860");
+		expectedResults.put("Suits S03E09 HDTV x264 EVOLVE AFG EVOLVE mSD HWD", "http://legendas.tv/pages/downloadarquivo/7b7a85a10c14185754a0c599a6e8163b");
+		expectedResults.put("Paranormal Witness S03E11 HDTV x264 INNOCENCE AFG INNOCENCE", "http://legendas.tv/pages/downloadarquivo/1d22e8cb551426fde59db248607b64fd");
+		expectedResults.put("Hell on Wheels S03E06 HDTV x264 EVOLVE AFG EVOLVE", "http://legendas.tv/pages/downloadarquivo/3be6ce89d4aa35652875358cf441badc");
 		subject.getNewer(2, new SubtitleLinkSearchCallback(){@Override public void process(final SubtitlePackageAndLink subAndLink) {
 			final String name = subAndLink.name;
 			final String link = subAndLink.link;

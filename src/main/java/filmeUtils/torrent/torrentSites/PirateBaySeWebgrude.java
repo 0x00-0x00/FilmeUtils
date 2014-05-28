b835a285a2767efa60877718ae5ea5c9dbf9ce64
Lucas <lucass@gmail.com>
2014-05-27 22:32:05 -0300

    Pirate bay está usando webgrude

--- src/main/java/filmeUtils/torrent/torrentSites/PirateBaySeWebgrude.java
+++ src/main/java/filmeUtils/torrent/torrentSites/PirateBaySeWebgrude.java
@@ -9,20 +9,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import webGrude.Browser;
 
-
 public class PirateBaySeWebgrude implements TorrentSite {
 
 	private static final String THEPIRATEBAY_SE_SEARCH_URL = "http://thepiratebay.se/search/"+TorrentSiteUtils.SEARCH_TERM_TOKEN+"/0/7/0";
-	private final SimpleHttpClient httpclient;
-
-	public PirateBaySeWebgrude(final SimpleHttpClient httpclient) {
-		this.httpclient = httpclient;
-	}
 
 	public String getMagnetLinkFirstResultOrNull(final String exactFileName) throws SiteOfflineException{
-        SearchResult tpb = Browser.open(SearchResult.class);
-        tpb.link.forEach( (e) -> System.out.println(e));
-		return null;
+        final String url = TorrentSiteUtils.getUrlFor(THEPIRATEBAY_SE_SEARCH_URL,exactFileName);
+        SearchResult tpb = Browser.open(url, SearchResult.class);
+        return tpb.link.get(0);
 	}
 
 	@Override

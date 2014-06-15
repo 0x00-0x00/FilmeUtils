b3a47303c75336b9ae8fdd22b7067917e49bb6f9
Lucas <lucass@gmail.com>
2014-06-14 23:54:34 -0300

    Esqueci de não logar debug

--- src/main/java/filmeUtils/Debug.java
+++ src/main/java/filmeUtils/Debug.java
@@ -4,6 +4,7 @@ public class Debug {
     public static final boolean IS_DEBUG = false;
 
     public static void log(String message){
-        System.out.println("[DEBUG] "+message);
+        if(IS_DEBUG)
+            System.out.println("[DEBUG] "+message);
     }
 }
